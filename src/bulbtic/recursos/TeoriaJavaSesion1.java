5550f4cad35d4040555c3ff0be3d474836afedea
Wilson <orochi_cris@yahoo.com>
2019-12-14 14:53:31 -0500

    actualizacion

--- src/bulbtic/recursos/TeoriaJavaSesion1.java
+++ src/bulbtic/recursos/TeoriaJavaSesion1.java
@@ -5,6 +5,8 @@
  */
 package bulbtic.recursos;
 
+import java.util.Arrays;
+
 import bulbtic.facturas.Factura;
 
 /**
@@ -18,8 +20,132 @@ public class TeoriaJavaSesion1 {
 	 *            the command line arguments
 	 */
 	public static void main(String[] args) {
-		Factura factura = new Factura();
-		boolean valor =  factura.verificacionFacturaActiva(true, null);
+
+		String[] nombres = { "Roberto", "Omar", "Julia", "Ana", "Ana" };
+		java.util.Arrays.sort(nombres);
+		int search = Arrays.binarySearch(nombres, "Ana");
+
+		int searchindex = 0;
+		for (String valor : nombres) {
+			System.out.println("INDICE: " + searchindex);
+			if (valor.equalsIgnoreCase("ana")) {
+				System.out.println("VALOR ENCONTRADO SEARCH FOR:" + valor);
+			}
+		}
+
+		System.out.println("SEACRH: " + search);
+
+		for (String valor : nombres)
+			System.out.println(valor);
+
+		boolean[] valorA = new boolean[2];
+		valorA[0] = true;
+		valorA[1] = true;
+
+		boolean[] valorA2 = new boolean[4];
+		valorA2[0] = false;
+		valorA2[2] = false;
+
+		System.arraycopy(valorA, 0, valorA2, 0, valorA.length);
+
+		int x1 = 0;
+		while (x1 < valorA2.length) {
+			System.out.println("VALOR ARRAYA BOOLEAN: " + valorA2[x1]);
+			x1++;
+		}
+
+		char valorcA[];
+		String valorsA[];
+		int[] valoriA;
+		double[] valordA;
+		float valorfA[];
+
+		Factura[] facturaA = { new Factura("Item", "Desc", 1, 12, 12, 12, 12, true),
+				new Factura("Item", "Desc", 1, 12, 12, 12, 12, true) };
+
+		for (int x = 0; x < 2; x++) {
+			System.out.println("Resultados: " + facturaA[x].getCantidad());
+		}
+
+		for (Factura f : facturaA) {
+			System.out.println("Resultados ForEach: " + f.getIva());
+		}
+
+		boolean[][] valorAA;
+		valorAA = new boolean[2][2];
+
+		valorAA[0][0] = true;
+		valorAA[0][1] = true;
+
+		valorAA[1][0] = false;
+		valorAA[1][1] = false;
+
+		// Filas
+		for (int x = 0; x < 2; x++) {
+			System.out.println("Impresion FILAS: " + valorAA[x][0]);
+		}
+
+		// columna
+		for (int y = 0; y < 2; y++) {
+			System.out.println("Impresion FILAS: " + valorAA[0][y]);
+		}
+
+		// fila columna
+		for (int x = 0; x < 2; x++) {
+			for (int y = 0; y < 2; y++) {
+				System.out.println("Impresion FILAS: " + valorAA[y][x]);
+			}
+		}
+
+		char valorcAA[][];
+		String valorsAA[][];
+		int[][] valoriAA;
+		double[][] valordAA;
+		float valorfAA[][];
+
+		Factura[][] facturaAA;
+
+		boolean valor = false;
+		char valorc = 'w';
+		String valors = "Wilson";
+		int valori = 2;
+		double valord = 2e3; // 2.00
+		float valorf = 2f; // 2.00
+		System.out.println("Datos Primitivos: " + "\nboolean:" + valor + "\nchar: " + valorc + "\nString: " + valors
+				+ "\nint: " + valori + "\ndouble: " + valord + "\nfloat: " + valorf);
+
+		float suma = valorf + valori;
+		System.out.println("SUMA ANTES: " + suma);
+		float incremento = suma++;
+		System.out.println("SUMA DESPUES: " + suma);
+		System.out.println("INCREMENTO: " + incremento);
+		float resta = valorf - valori;
+		System.out.println("RESTA: " + resta);
+		float mult = valorf * valori;
+		System.out.println("MULT: " + mult);
+		float div = valorf / valori;
+		System.out.println("DIVISION: " + div);
+		float resto = valorf % valori;
+		System.out.println("RESTO: " + resto);
+
+		if (!((3 <= 2) || (2 >= 3))) {
+			System.out.println("es mayor");
+		}
+		if (3 >= 2) {
+			System.out.println("es menor");
+		}
+		if (3 == 2) {
+			System.out.println("es igual");
+		}
+		if (3 != 2) {
+			System.out.println("no es igual");
+		}
+
+		int x = 'w';
+		int y = x << 2;
+
+		System.out.println("DESPLAZAMINETOS:\nValor x: " + x + "\n" + "Valor y: " + y);
+
 	}
 
 	void metodo1() {
